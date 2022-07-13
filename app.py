5921793c86f883d07cc75df67cb4548d70125f38
Minsuk Kim <msk7316@gmail.com>
2022-07-13 17:23:04 +0900

    수정중

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import re
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 from pymongo import MongoClient
 from bs4 import BeautifulSoup
 import certifi
@@ -7,36 +7,29 @@ ca = certifi.where()
 import requests
 app = Flask(__name__)
 
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+
+import jwt
+import datetime
+import hashlib
+
+SECRET_KEY = 'SPARTA'
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
 client = MongoClient('mongodb+srv://test:sparta@cluster0.ehdakrl.mongodb.net/?retryWrites=true&w=majority',tlsCAFile=ca)
 db = client.dbsparta
 
-#####로그인(인덱스) 페이지 렌더
-@app.route('/')
-def main():
-    return render_template("index.html")
 
-#####회원가입 페이지 렌더
-@app.route('/signup')
-def signup():
 
-    return render_template("signup.html")
 
 #####추천 리스트 페이지 렌더
 @app.route('/recommendList')
 def recommendList():
 
     return render_template("recommendList.html")
-
-#####마이페이지 렌더
-@app.route('/mypage')
-def mypage():
-    return render_template("mypage.html")
-
-
-
-
-
-
 
 ##########################################
 #####임시 디비에 추가하는 페이지
@@ -117,54 +110,7 @@ def like_cancel():
     return jsonify({'msg': '좋아요 취소'})
 
 
-
-
-
-#####로그인(인덱스) 페이지 API
-@app.route('/api/login', methods=['POST'])
-def login():
-
-    return jsonify("로그인 완료")
-
-#####프로필 API
-@app.route('/api/profile', methods=['POST'])
-def profile():
-    return jsonify({"프로필 편집 완료"})
-
-#####마이페이지 API
-@app.route('/api/mypage', methods=['GET'])
-def mypage_get():
-    return jsonify({"프로필, 개인 플레이 리스트 조회"})
-
-#####플레이 리스트 API
-@app.route('/api/recommendList', methods=['GET'])
-def playlist_get():
-
-    return jsonify({"체크된 태그 달린 플레이 리스트 조회"})
-
-
-
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
-
 # ###########################################구분선 #########################################
-
-
-import jwt
-import datetime
-import hashlib
-from flask import Flask, render_template, jsonify, request, redirect, url_for
-from datetime import datetime, timedelta
-
-app = Flask(__name__)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-
-SECRET_KEY = 'SPARTA'
-
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://test:sparta@cluster0.bpwj4ks.mongodb.net/Cluster0?retryWrites=true&w=majority")
-db = client.oneweek
 
 
 @app.route('/')
@@ -173,7 +119,7 @@ def home():
     try:
         payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
         user_info = db.users.find_one({"username": payload["id"]})
-        return render_template('index.html', user_info=user_info)
+        return render_template('tempInsertPage.html', user_info=user_info)
 
     except jwt.ExpiredSignatureError:
         return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
@@ -209,21 +155,6 @@ def sign_in():
         return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
-@app.route('/sign_up/save', methods=['POST'])
-def sign_up():
-    username_receive = request.form['username_give']
-    password_receive = request.form['password_give']
-    profile_name_receive = request.form['profile_name_give']
-    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    doc = {
-        "username": username_receive,                               # 아이디
-        "password": password_hash,                                  # 비밀번호
-        "profile_name": profile_name_receive,                       # 프로필 이름
-    }
-    db.users.insert_one(doc)
-    return jsonify({'result': 'success'})
-
-
 @app.route('/sign_up/check_dup', methods=['POST'])
 def check_dup():
     username_receive = request.form['username_give']
@@ -238,50 +169,7 @@ def check_dup2():
 
 
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
-
-
-
-
-
 # ###########################################구분선 #########################################
-
-from pymongo import MongoClient
-import jwt
-import datetime
-import hashlib
-from flask import Flask, render_template, jsonify, request, redirect, url_for
-from werkzeug.utils import secure_filename
-from datetime import datetime, timedelta
-
-app = Flask(__name__)
-app.config["TEMPLATES_AUTO_RELOAD"] = True
-app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
-
-SECRET_KEY = 'SPARTA'
-
-client = MongoClient('mongodb+srv://test:sparta@cluster0.myx5l74.mongodb.net/Cluster0?retryWrites=true&w=majority')
-db = client.dbsparta_plus_week4
-
-
-@app.route('/')
-def home():
-    token_receive = request.cookies.get('mytoken')
-    try:
-        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-        user_info = db.users.find_one({"username": payload["id"]})
-        return render_template('index.html', user_info=user_info)
-    except jwt.ExpiredSignatureError:
-        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
-    except jwt.exceptions.DecodeError:
-        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
-
-
-@app.route('/login')
-def login():
-    msg = request.args.get("msg")
-    return render_template('login.html', msg=msg)
 
 
 @app.route('/user/<username>')
@@ -297,27 +185,6 @@ def user(username):
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
         return redirect(url_for("home"))
 
-
-@app.route('/sign_in', methods=['POST'])
-def sign_in():
-    # 로그인
-    username_receive = request.form['username_give']
-    password_receive = request.form['password_give']
-
-    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
-    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
-
-    if result is not None:
-        payload = {
-            'id': username_receive,
-            'exp': datetime.utcnow() + timedelta(seconds=60 * 60 * 24)  # 로그인 24시간 유지
-        }
-        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-
-        return jsonify({'result': 'success', 'token': token})
-    # 찾지 못하면
-    else:
-        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
 
 
 @app.route('/sign_up/save', methods=['POST'])
@@ -336,12 +203,6 @@ def sign_up():
     db.users.insert_one(doc)
     return jsonify({'result': 'success'})
 
-
-@app.route('/sign_up/check_dup', methods=['POST'])
-def check_dup():
-    username_receive = request.form['username_give']
-    exists = bool(db.users.find_one({"username": username_receive}))
-    return jsonify({'result': 'success', 'exists': exists})
 
 
 @app.route('/update_profile', methods=['POST'])
